943d05c35a49532a56f09aa0b3be5e1e02936d08
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-09-08 20:57:45 -0300

    Simple REST API with Razor pages

---
+++ CodeProject/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodeProject.Models;
+
+namespace CodeProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Empleado>(table =>
+            {
+                table.HasKey(col => col.IdEmpleado);
+
+                table.Property(col => col.IdEmpleado)
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                table.Property(col => col.NombreCompleto).HasMaxLength(50);
+                table.Property(col => col.Correo).HasMaxLength(50);
+
+            });
+
+            modelBuilder.Entity<Empleado>().ToTable("Empleado");
+
+        }
+    }
+}
